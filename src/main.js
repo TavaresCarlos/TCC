a51f21a294301bf645a5244413300d8fddf7ca39
TavaresCarlos <caca_cht@hotmail.com>
2020-12-23 10:18:01 -0300

    Axios e Vue router adicionados

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Axios from 'axios'
+
+Vue.prototype.$http = Axios;
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	history: true,
+	routes: [{
+
+	}]
+})
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+	router,
+  	el: '#app',
+  	render: h => h(App)
 })
