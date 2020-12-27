fb2051a5de007288f0cbdd322d794600df762253
Carlos Henrique Tavares <tavares.carlos.h@gmail.com>
2020-12-27 10:47:42 -0300

    Cadastro de novos usuários funcionando

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
 const Pool = require('pg').Pool;
 
 const app = express();
@@ -10,6 +13,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }))
 
 const pool = new Pool({
 	user: 'postgres',
@@ -19,7 +23,7 @@ const pool = new Pool({
 	port: 5432,
 });
 
-function teste(sql, res) {
+function executaSql(sql, res) {
 	pool.query(sql, (error, results) => {
 		if(error){
 			res.json(error);
@@ -33,7 +37,23 @@ function teste(sql, res) {
 //Rotas
 app.get('/', (req, res) => {
 	//res.json({ message: 'Funcionando' })
-	teste('SELECT * FROM usuario', res);
+	executaSql('SELECT * FROM usuario', res);
+});
+
+app.post('/cadastroNovoUsuario', (req, res) => {
+	console.log("Rota chamada");
+
+	const nome = req.body.nome;
+	const apelido = req.body.apelido;
+	const faixaEtaria = req.body.faixaEtaria;
+	const email = req.body.email;
+	const senha = bcrypt.hashSync(req.body.senha, 10);
+	const tipo = 'colaborador';
+
+	var query = `INSERT INTO usuario (nome, apelido, email, senha, faixaEtaria, tipo) VALUES ( '${nome}', '${apelido}', '${email}', '${senha}', '${faixaEtaria}', '${tipo}' )`;
+	executaSql(query, res);
+
+	console.log(query);
 });
 
 //Rodando o servidor
