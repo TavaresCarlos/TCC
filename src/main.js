89b07512f3ab4afceecbd5db78b4936300c43655
Carlos Henrique Tavares <tavares.carlos.h@gmail.com>
2021-01-03 09:51:57 -0300

    Ideia de interface

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import Exportar from './componentes/Exportar.vue'
 import PainelUsuario from './componentes/PainelUsuario.vue'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = new VueRouter({
 	history: true,
